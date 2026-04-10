714bb68402e4257f138393ef1b09b5de556305a1
dead-Spy <tanvirheaven199@gmail.com>
2026-04-10 22:27:55 +0600

    chrome: update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,12 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
 
 starship init fish | source
 
-function up
-    cd ~/dotfiles
-    git add .
-    
-    if count $argv > /dev/null
-        git commit -m "$argv"
-    else
-        git commit -m "sync: updated on (date '+%Y-%m-%d %H:%M:%S')"
-    end
-    
-    git push
-    cd -
-end
 if test -f ~/.config/fish/weather_key.fish
     source ~/.config/fish/weather_key.fish
 end
+
 set -g fish_greeting ""
 fastfetch
-
